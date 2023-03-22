f0a8df0594f19077e106a65be73f0851bf06b391
tuncduluc <tuncduluc@gmail.com>
2023-03-22 06:42:39 +0300

    added fileicons

--- splitting/script.js
+++ splitting/script.js
@@ -1,4 +1,5 @@
 let nameCounter = {};
+const fileIconUrl = "fileicon.png";
 
 function generateName() {
   const input = document.getElementById("nameInput").value;
@@ -6,10 +7,30 @@ function generateName() {
   const extension = parts.pop();  // here we are removing the last element of the array. and call it "extension"
                                   // for example if we put "this.is.my.pdf", we removed the "pdf" with "pop()"".   
   const name = parts.join(".");  // then we rejoin the rest of the elements by using "join(".")"  -->  "this.is.my"
+  
   let count = nameCounter[name] || 0;
   count++;
   nameCounter[name] = count;
   const newName = name + "-" + count + "." + extension;  //here we recall the "extension" and add it again.
-                                                       
+  
+  // created a new image element for the file icon
+  const fileIcon = document.createElement("img");
+  fileIcon.src = fileIconUrl;
+  fileIcon.alt = "File Icon";
+  fileIcon.style.marginRight = "5px";
+
+  // created a new span element to hold the file name 
+  const fileName = document.createElement("span");
+  fileName.innerText = newName;
+
+  // created a new div element to hold the file icon and file name
+  const fileContainer = document.createElement("div");
+  fileContainer.appendChild(fileIcon);
+  fileContainer.appendChild(fileName);
+
+  // where the file containers will be displayed
+  const fileSection = document.getElementById("fileSection");
+  fileSection.appendChild(fileContainer);
+
   console.log(newName);
 }
